dc215e554044feb885b7489d27dd69fee8e710d6
codez1995 <tarun1995bly@gmail.com>
2022-05-03 18:40:45 +0530

    add funcitonality

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -8,10 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class ToDoListComponent implements OnInit {
 
   listDatacolumns = [
-    {name: "taskName", displayName: "TASK NAME", width: "100px", type:"text", class:"task"},
-    {name: "taskDescription", displayName: "TASK DESCRIPTION", width: "500px", type:"text", class:"task"},
-    {name: "repeatTask", displayName: "REPEAT TASK", width:"100px", type:"text", class:"task"},
-    {name: "icons", displayName: "ICON", width:"100px", type:"icon", class:"task"}
+    {name: "taskName", displayName: "TASK NAME", width: "200px", type:"text", class:"task"},
+    {name: "taskDescription", displayName: "TASK DESCRIPTION", width: "800px", type:"text", class:"task"},
+    {name: "repeatTask", displayName: "REPEAT TASK", width:"150px", type:"text", class:"task"},
+    {name: "icons", displayName: "ICON", width:"200px", type:"icon", class:"task"}
   ]
   moviesData!: any[];
   listDisplayColumns!: string[];
--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-interface Food {
+interface Task {
   value: string;
   viewValue: string;
 }
@@ -13,14 +14,21 @@ interface Food {
 
 export class ToDoComponent implements OnInit {
 
-  foods: Food[] = [
-    {value: 'steak-0', viewValue: 'Steak'},
-    {value: 'pizza-1', viewValue: 'Pizza'},
-    {value: 'tacos-2', viewValue: 'Tacos'},
+  tasks: Task[] = [
+    {value: 'yes', viewValue: 'YES'},
+    {value: 'no', viewValue: 'NO'},
   ];
+  taskForm!: FormGroup;
   constructor() { }
 
   ngOnInit(): void {
+    this.InitializetaskForm();
   }
-
+  InitializetaskForm() {
+    this.taskForm = new FormGroup({
+      taskName: new FormControl('', [Validators.required]),
+      taskDescription: new FormControl('', [Validators.required]),
+      repeatTask: new FormControl('', [Validators.required]),
+    })
+  }
 }
