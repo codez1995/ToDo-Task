3b7064a084528e0171c2a87cd3005a3bbf6e11bb
codez1995 <tarun1995bly@gmail.com>
2022-05-03 15:22:27 +0530

    Upload ToDo, ToDoList, and DoneList Component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToDoComponent } from './to-do/to-do.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
+import { DoneListComponent } from './done-list/done-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoComponent,
+    ToDoListComponent,
+    DoneListComponent
   ],
   imports: [
     BrowserModule,
